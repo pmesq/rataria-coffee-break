50ccaa315341eab4eee9ea6752e253237df6e01a
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2023-06-22 14:18:24 -0300

    adicionado menu principal e troca de modo

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import pygame
+
 class Config:
     """
     Classe que armazena configurações e constantes relacionadas ao jogo.
     Contém atributos estáticos com valores específicos.
     """
+
+    pygame.init()
 
     # Configurações da tela
     SCREEN_WIDTH = 1280
@@ -12,6 +16,13 @@ class Config:
     # Configurações dos blocos
     BLOCK_SIZE = 80
     BLOCK_COLOR = "#222222"
-
     # Configuração do jogador
     PLAYER_COLOR = "purple"
+
+    # Configuração da fonte
+    font = pygame.font.SysFont("arialblacomicck", 40)
+    COR_FONTE = (255, 255, 255)
+    COR_FUNDO_TEXTO = (0, 0, 0)
+
+    # imagens que serão usadas durante o jogo
+    menu1_jpg = pygame.image.load("imgs/menu1.jpg")
--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from level_reader import LevelReader
 from rat import Rat
 from player import Player
 
+from enum import Enum
+
+# Enum utilizado para simbolizar em qual tela está o jogador 
+class Tela(Enum):
+    JOGO = 1
+    MENU_PRINCIPAL= 2
+    MENU_INICIAL = 3
+    LEADERBOARD = 4
+
+    # Setter: Define o num
+    def setter(self, pos):
+        self = pos
+
 pygame.init()
 
 # Configuração da tela do jogo
@@ -26,16 +39,40 @@ tilemap, player, bodies = LevelReader.read('data/levels/1.txt')
 # Variável de controle do loop principal do jogo
 running = True
 
-# Loop principal do jogo
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# Variável que controla em qual tela o jogo esta 
+telaAtual = Tela.MENU_INICIAL
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+def menu_inicial(keys):
+    # Declaro que usarei a variaǘel local telaAnual
+    global telaAtual
+
+    if keys[pygame.K_SPACE]:
+        telaAtual = Tela.JOGO
+        return
     
-    screen.fill(Config.SCREEN_COLOR) # Preenchimento da tela com uma cor de fundo    
-    keys = pygame.key.get_pressed() # Captura das teclas pressionadas pelo jogador
     
-    # Verificação das teclas de movimentação do jogador
+    screen.blit(Config.menu1_jpg, (0,0))
+
+    pygame.draw.rect(screen, Config.COR_FONTE, (359, 432, 516, 56))
+    pygame.draw.rect(screen, Config.COR_FUNDO_TEXTO, (362, 435, 510, 50))
+    draw_text("Pressione ESPAÇO para iniciar", Config.font, Config.COR_FONTE, 407, 450)
+
+    telaAtual = Tela.MENU_INICIAL
+    return
+
+def jogo(keys):
+    # Declaro que usarei a variável local telaAnual
+    global telaAtual
+
+    # Caso seja pressionado a telca ESC, eu troco para o modo do menu 
+    if keys[pygame.K_ESCAPE]:
+        telaAtual = Tela.MENU_INICIAL
+        return
+
     if keys[pygame.K_w]:
         player.jump()
     if keys[pygame.K_a] and not keys[pygame.K_d]:
@@ -52,6 +89,25 @@ while running:
     
     # Atualização da posição da câmera para seguir o jogador
     camera.pos.x = min(max(0, player.pos.x - Config.SCREEN_WIDTH / 2), tilemap.m * Config.BLOCK_SIZE - Config.SCREEN_WIDTH)
+
+    # Continuo no modo jogo
+    telaAtual = Tela.JOGO
+
+# Loop principal do jogo
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(Config.SCREEN_COLOR) # Preenchimento da tela com uma cor de fundo    
+    keys = pygame.key.get_pressed() # Captura das teclas pressionadas pelo jogador
+    
+    # Executo a função caracteristica do modo atual
+    if telaAtual == Tela.MENU_INICIAL:
+        menu_inicial(keys)
+    elif telaAtual == Tela.JOGO:
+        jogo(keys)
+    
+    print(telaAtual)
 
     # Atualização da tela e controle de FPS
     pygame.display.flip()
