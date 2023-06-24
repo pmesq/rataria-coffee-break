80d06526226f1fcd2e7800d96d7a4254dab6bbfd
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2023-06-24 19:33:47 -0300

    adicionado a funcao leaderboard, falta fazer o gui dela

---
+++ leaderboard.py
@@ -0,0 +1,54 @@
+import pygame
+import heapq
+#from config import Config
+#from evento import Evento
+
+class Leaderboard():
+	def __init__(self, endrecoArquivo):
+		self.listaRecordes = {}
+
+		with open(endrecoArquivo, 'r') as file:
+			linhas = file.read()
+			linhas = linhas.split('\n')
+			for linha in linhas:
+				if(linha != "FIM"):
+					linha_dividida = linha.split(";")
+					self.adicionarJogador(linha_dividida[0], int(linha_dividida[1]))
+			file.close()
+			pass
+
+	def imprimir(self):
+		listaTop5 = self.lisaTop5Jogadore()
+		for chave, valor in listaTop5:
+			print(f"{chave} {valor}")
+
+	def lisaTop5Jogadore(self):
+		listaTop5 = heapq.nlargest(5, self.listaRecordes.items(), key=lambda item: item[1])
+		return listaTop5
+		
+
+	def adicionarJogador(self, nomeJogador, ponutacaoJogador):
+		if nomeJogador not in self.listaRecordes:
+			self.listaRecordes[nomeJogador] = int(ponutacaoJogador)
+		else:
+			self.listaRecordes[nomeJogador] = max(int(self.listaRecordes[nomeJogador]), int(ponutacaoJogador))
+
+	def gravar(self, endrecoArquivo):
+		listaTop5 = self.lisaTop5Jogadore()
+		with open(endrecoArquivo, 'w') as File:
+			for nomeJogador, pontosJogador in listaTop5:
+				File.write(nomeJogador+";"+str(pontosJogador)+"\n")
+			File.write("FIM")
+			File.close()
+			pass
+		
+'''
+	def draw(self, screen):
+		lista = self.lisaTop5Jogadores()
+		sizePlacar = lista.size()
+
+		for idx in range(0, 5):
+			if(sizePlacar > idx):
+				Evento.draw_text(screen, str((idx+1)+". "+lista[idx]), Config.font, Config.COR_FONTE, 100, 100+20*idx)
+		
+'''	
